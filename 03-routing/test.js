a2e572583269c3ad097b9e73b4a501024432e69f
chpill <etienne@spillemaeker.com>
2015-01-16 18:06:29 +0100

    Got all the way to error handling!

--- 03-routing/test.js
+++ 03-routing/test.js
@@ -13,12 +13,14 @@ describe('Routing', function () {
   it('GET /404 should return "page not found"', function (done) {
     request(app.listen())
     .get('/404')
+    .expect(404)
     .expect('page not found', done);
   })
 
   it('get /500 should return "internal server error"', function (done) {
     request(app.listen())
     .get('/500')
+    .expect(500)
     .expect('internal server error', done);
   })
 })
